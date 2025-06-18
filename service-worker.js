8e8602c8af9407e151acee11c0ca2d019018e2da
R M Sai Likhita <rm.sailikhita1@gmail.com>
2025-06-18 17:56:15 +0530

    updated website

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "my-portal-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/live.html",
+  "/categories.html",
+  "/login.html",
+  "/style.css",
+  "/stylelive.css",
+  "/js/countdown.js",
+  "/categories.js",
+  "/feature.js",
+  "/manifest.json",
+  "/act.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("/index.html");
+        }
+      });
+    })
+  );
+});
