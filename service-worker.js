d5b313ad011cf6f2d94a9caf11453e77f081fafb
rugved18 <rugvedkulk2003@gmail.com>
2025-06-18 13:36:16 +0530

    pwa updated

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "auction-cache-v1";
+const urlsToCache = [
+  "/index.html",
+  "/live.html",
+  "/login.html",
+  "/style.css",
+  "/stylelive.css",
+  "/js/countdown.js",
+  "/feature.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
