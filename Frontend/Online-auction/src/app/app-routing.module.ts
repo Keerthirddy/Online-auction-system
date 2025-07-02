9adec42e29ac3ab3c01c832082abc49fba0aa097
R M Sai Likhita <rm.sailikhita1@gmail.com>
2025-07-02 22:52:25 +0530

    routing-live

--- Frontend/Online-auction/src/app/app-routing.module.ts
+++ Frontend/Online-auction/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CategoriesComponent } from './components/categories/categories.component';
-import { LiveAuctionsComponent } from './components/live-auction/live-auction.component';
 
 const routes: Routes = [
-   { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'live-auctions', component: LiveAuctionsComponent },
+  { 
+    path: 'auction', 
+    loadChildren: () => import('./components/live-auction/live-auction.module').then(m => m.LiveAuctionModule)
+  },
   { path: 'categories', component: CategoriesComponent },
   { path: '**', redirectTo: '' }
 ];
