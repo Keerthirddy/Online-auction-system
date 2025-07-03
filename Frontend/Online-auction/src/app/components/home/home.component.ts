f6f0614da988d410635b647bb4804d306f103a08
NoorSaniyaa <nooorsaniya@gmail.com>
2025-07-03 17:30:44 +0530

    fix: apply ESLint fixes to HomeComponent

--- Frontend/Online-auction/src/app/components/home/home.component.ts
+++ Frontend/Online-auction/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuctionService } from '../../services/auction.service';
 import { CategoryService } from '../../services/category.service';
@@ -8,54 +8,53 @@ import { Category } from '../../models/category.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   featuredAuctions: Auction[] = [];
   ongoingAuctions: Auction[] = [];
   categories: Category[] = [];
-  searchQuery: string = '';
+  searchQuery = '';
   loading = true;
 
-  constructor(
-    private auctionService: AuctionService,
-    private categoryService: CategoryService,
-    private router: Router
-  ) { }
+  private auctionService = inject(AuctionService);
+  private categoryService = inject(CategoryService);
+  private router = inject(Router);
 
-  ngOnInit(): void {
+  constructor() {
     this.loadFeaturedAuctions();
     this.loadOngoingAuctions();
     this.loadCategories();
   }
 
   private loadFeaturedAuctions(): void {
-    this.auctionService.getFeaturedAuctions().subscribe(auctions => {
+    this.auctionService.getFeaturedAuctions().subscribe((auctions) => {
       this.featuredAuctions = auctions;
       this.loading = false;
     });
   }
 
   private loadOngoingAuctions(): void {
-    this.auctionService.getOngoingAuctions().subscribe(auctions => {
+    this.auctionService.getOngoingAuctions().subscribe((auctions) => {
       this.ongoingAuctions = auctions;
     });
   }
 
   private loadCategories(): void {
-    this.categoryService.getCategories().subscribe(categories => {
+    this.categoryService.getCategories().subscribe((categories) => {
       this.categories = categories;
     });
   }
 
   onCategoryClick(category: Category): void {
-    this.router.navigate(['/categories'], { queryParams: { category: category.key } });
+    this.router.navigate(['/categories'], {
+      queryParams: { category: category.key },
+    });
   }
 
   onSearch(): void {
     if (this.searchQuery.trim()) {
-      // In a real app, this would navigate to search results
-      console.log('Searching for:', this.searchQuery);
+      //console.log('Searching for:', this.searchQuery); 
     }
   }
-} 
+}
