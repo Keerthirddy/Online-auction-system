9bcd56373105dd40f8bbb45b2574c190b67952e4
MuthakanaHinduja <hindujm04@gmail.com>
2025-07-03 13:13:55 +0530

    bid form

---
+++ Frontend/Online-auction/src/app/components/bidding-form/bidding-form.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BiddingFormComponent } from './bidding-form.component';
+
+describe('BiddingFormComponent', () => {
+  let component: BiddingFormComponent;
+  let fixture: ComponentFixture<BiddingFormComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BiddingFormComponent]
+    });
+    fixture = TestBed.createComponent(BiddingFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
